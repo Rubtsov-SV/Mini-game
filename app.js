bfa1115d8049b460b54df007034c10d3bcc197c3
Сергей <serega16.92@inbox.ru>
2023-04-07 11:40:04 +0500

    Рефакторинг

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ for(let i = 0; i < SQUARES_NUMBER; i++) {
   square.classList.add('square');
 
   square.addEventListener('mouseover', () => {
-    setColor(square)
-  })
+    setColor(square);
+  });
 
   square.addEventListener('mouseleave', () => {
-    removeColor(square)
-  })
+    removeColor(square);
+  });
 
   board.append(square);
 }
@@ -20,15 +20,15 @@ for(let i = 0; i < SQUARES_NUMBER; i++) {
 function setColor(element) {
  const color =  getRandomColor();
  element.style.backgroundColor = color;
- element.style.boxShadow = `0 0 2px ${color}`
-}
+ element.style.boxShadow = `0 0 2px ${color}`;
+};
 
 function removeColor(element) {
   element.style.backgroundColor = '#1d1d1d';
-  element.style.boxShadow = '0 0 2px #000'
+  element.style.boxShadow = '0 0 2px #000';
 }
 
 function getRandomColor() {
   const index = Math.floor(Math.random() * colors.length);
-  return colors[index]
-}
+  return colors[index];
+};
